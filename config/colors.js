38b0d8bac6c63f2e9fcbc337d95e3fcc4ecffcb7
ushagour <ali.ouchagour01@gmail.com>
2025-11-14 21:14:51 +0100

    add colors

---
+++ config/colors.js
@@ -0,0 +1,28 @@
+export default {
+  primary1: "#495E57",     // Ochre, main accent color
+  primary2: "#F4CE14",     // Ochre, main accent color
+  secondary1: "#EE9972",
+  secondary2: "#FBDABB",
+  secondary3: "#333333",
+  secondary4: "#EDEFEE",
+  secondary5: "#A1A4AD",
+  secondary6: "#D9D9D9",
+
+  black: "#000",
+  white: "#fff",
+  medium: "#6e6969",
+  light: "#F9F4EF",
+  dark: "#0c0c0c",
+  danger: "#ff5252",
+  warning: "#ffc107",
+  success: "#4BB543",
+  textPrimary: "#495E57", // Dark charcoal, for main text
+  textSecondary: "#EE9972", // Gray, for secondary text or hints
+
+  background: "#F9F4EF",  // Light beige, a soft background color
+  shadowColor: "#00000020" 
+   
+
+};
+
+
